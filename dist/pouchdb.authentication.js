9fc360fe97677c15b3c27146e979a1a8eb3b3e42
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-01 21:13:42 -0400

    build

--- dist/pouchdb.authentication.js
+++ dist/pouchdb.authentication.js
@@ -16,6 +16,28 @@ function wrapError(callback) {
   };
 }
 
+function putUser(db, user, opts, callback) {
+  var reservedWords = ['name', 'password', 'roles', 'type', 'salt', 'metadata'];
+  if (opts.metadata) {
+    for (var key in opts.metadata) {
+      if (opts.hasOwnProperty(key)) {
+        if (reservedWords.indexOf(key) !== -1 || key.startsWith('_')) {
+          return callback(new AuthError('cannot use reserved word in metadata: "' + key + '"'));
+        }
+      }
+    }
+    user = utils.extend(true, user, opts.metadata);
+  }
+
+  var url = utils.getUsersUrl(db) + '/' + encodeURIComponent(user._id);
+  var ajaxOpts = utils.extend(true, {
+    method : 'PUT',
+    url : url,
+    body : user
+  }, opts.ajax || {});
+  utils.ajax(ajaxOpts, wrapError(callback));
+}
+
 exports.signup = utils.toPromise(function (username, password, opts, callback) {
   var db = this;
   if (typeof callback === 'undefined') {
@@ -24,7 +46,7 @@ exports.signup = utils.toPromise(function (username, password, opts, callback) {
     opts = {};
   }
   if (['http', 'https'].indexOf(db.type()) === -1) {
-    return callback(new AuthError('This plugin only works for the http/https adapter. ' + 
+    return callback(new AuthError('This plugin only works for the http/https adapter. ' +
       'So you should use new PouchDB("http://mysite.com:5984/mydb") instead.'));
   } else if (!username) {
     return callback(new AuthError('You must provide a username'));
@@ -41,25 +63,7 @@ exports.signup = utils.toPromise(function (username, password, opts, callback) {
     _id      : userId
   };
 
-  var reservedWords = ['name', 'password', 'roles', 'type', 'salt', 'metadata'];
-  if (opts.metadata) {
-    for (var key in opts.metadata) {
-      if (opts.hasOwnProperty(key)) {
-        if (reservedWords.indexOf(key) !== -1 || key.startsWith('_')) {
-          return callback(new AuthError('cannot use reserved word in metadata: "' + key + '"'));
-        }
-      }
-    }
-    user = utils.extend(true, user, opts.metadata);
-  }
-
-  var url = utils.getUsersUrl(db) + '/' + encodeURIComponent(userId);
-  var ajaxOpts = utils.extend(true, {
-    method : 'PUT',
-    url : url,
-    body : user
-  }, opts.ajax || {});
-  utils.ajax(ajaxOpts, wrapError(callback));
+  putUser(db, user, opts, callback);
 });
 
 exports.signUp = exports.signup;
@@ -84,7 +88,7 @@ exports.login = utils.toPromise(function (username, password, opts, callback) {
     method : 'POST',
     url : utils.getSessionUrl(db),
     headers : {'Content-Type': 'application/x-www-form-urlencoded'},
-    body : 'name=' + username + '&password=' + password
+    body : 'name=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password)
   }, opts.ajax || {});
   utils.ajax(ajaxOpts, wrapError(callback));
 });
@@ -139,6 +143,28 @@ exports.getUser = utils.toPromise(function (username, opts, callback) {
   utils.ajax(ajaxOpts, wrapError(callback));
 });
 
+exports.putUser = utils.toPromise(function (username, opts, callback) {
+  var db = this;
+  if (typeof callback === 'undefined') {
+    callback = typeof opts === 'undefined' ? username : opts;
+    opts = {};
+  }
+  if (['http', 'https'].indexOf(db.type()) === -1) {
+    return callback(new AuthError('This plugin only works for the http/https adapter. ' +
+      'So you should use new PouchDB("http://mysite.com:5984/mydb") instead.'));
+  } else if (!username) {
+    return callback(new AuthError('You must provide a username'));
+  }
+
+  return db.getUser(username, opts, function (error, user) {
+    if (error) {
+      return callback(error);
+    }
+
+    putUser(db, user, opts, callback);
+  });
+});
+
 exports.changePassword = utils.toPromise(function (username, password, opts, callback) {
   var db = this;
   if (typeof callback === 'undefined') {
@@ -147,7 +173,7 @@ exports.changePassword = utils.toPromise(function (username, password, opts, cal
     opts = {};
   }
   if (['http', 'https'].indexOf(db.type()) === -1) {
-    return callback(new AuthError('This plugin only works for the http/https adapter. ' + 
+    return callback(new AuthError('This plugin only works for the http/https adapter. ' +
       'So you should use new PouchDB("http://mysite.com:5984/mydb") instead.'));
   } else if (!username) {
     return callback(new AuthError('You must provide a username'));
@@ -172,6 +198,66 @@ exports.changePassword = utils.toPromise(function (username, password, opts, cal
   });
 });
 
+exports.changeUsername = utils.toPromise(function (oldUsername, newUsername, opts, callback) {
+  var db = this;
+  var USERNAME_PREFIX = 'org.couchdb.user:';
+  var ajax = function (opts) {
+    return new utils.Promise(function (resolve, reject) {
+      utils.ajax(opts, wrapError(function (err, res) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(res);
+      }));
+    });
+  };
+  var updateUser = function (user, opts) {
+    var url = utils.getUsersUrl(db) + '/' + encodeURIComponent(user._id);
+    var updateOpts = utils.extend(true, {
+      method : 'PUT',
+      url : url,
+      body: user
+    }, opts.ajax);
+    return ajax(updateOpts);
+  };
+  if (typeof callback === 'undefined') {
+    callback = opts;
+    opts = {};
+  }
+  opts.ajax = opts.ajax || {};
+  if (['http', 'https'].indexOf(db.type()) === -1) {
+    return callback(new AuthError('This plugin only works for the http/https adapter. ' +
+      'So you should use new PouchDB("http://mysite.com:5984/mydb") instead.'));
+  }
+  if (!newUsername) {
+    return callback(new AuthError('You must provide a new username'));
+  }
+  if (!oldUsername) {
+    return callback(new AuthError('You must provide a username to rename'));
+  }
+
+  return db.getUser(newUsername, opts)
+  .then(function () {
+    var error = new AuthError('user already exists');
+    error.taken = true;
+    throw error;
+  }, function () {
+    return db.getUser(oldUsername, opts);
+  })
+  .then(function (user) {
+    var newUser = utils.clone(user);
+    delete newUser._rev;
+    newUser._id = USERNAME_PREFIX + newUsername;
+    newUser.name = newUsername;
+    newUser.roles = opts.roles || user.roles || {};
+    return updateUser(newUser, opts).then(function () {
+      user._deleted = true;
+      return updateUser(user, opts);
+    });
+  }).then(function (res) {
+    callback(null, res);
+  }).catch(callback);
+});
 
 
 function AuthError(message) {
