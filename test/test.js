9529f0c63e523f5f6716b3f8742a2a67eb8792fe
Didier Villevalois (Ptitjes) <ptitjes@free.fr>
2017-11-14 02:50:01 +0100

    fix(putUser): ensure reserved words are enforced in metadata
    BREAKING CHANGE: In both signUp and putUser, '_id', '_rev', 'name',
    'type', 'roles', 'password', 'password_scheme', 'iterations',
    'derived_key', 'salt' are now all reserved words, and 'metadata' is
    not a reserved word anymore.

--- test/test.js
+++ test/test.js
@@ -22,8 +22,8 @@ testCases.forEach(function (testCase) {
   describe('authentication-' + testCase, function () {
 
     var dbName = testCase === 'normal' ?
-      'http://localhost:5984/testdb' :
-      'http://localhost:5984/testdb/'; // trailing slash
+        'http://localhost:5984/testdb' :
+        'http://localhost:5984/testdb/'; // trailing slash
 
     var db;
 
@@ -141,6 +141,73 @@ testCases.forEach(function (testCase) {
         user.name.should.equal('robin');
         user.alias.should.equal('rednowyob');
         user.profession.should.equal('taborca');
+      });
+    });
+
+    var reservedWords = [
+      '_id',
+      '_rev',
+      'name',
+      'type',
+      'roles',
+      'password',
+      'password_scheme',
+      'iterations',
+      'derived_key',
+      'salt'
+    ];
+
+    reservedWords.forEach(function (key) {
+      it('Test changing metadata using reserved word "' + key + '"', function () {
+        return db.signup('robin', 'dickgrayson').then(function (res) {
+          res.ok.should.equal(true);
+          return db.login('robin', 'dickgrayson');
+        }).then(function () {
+          return db.getUser('robin').then(function (user) {
+            var metadata = {};
+            metadata[key] = 'test';
+            return db.putUser('robin', {metadata: metadata}).then(function (res) {
+              res.ok.should.not.equal(true);
+            }).catch(function (err) {
+              should.exist(err);
+              err.status.should.equal(400);
+              err.name.should.equal('authentication_error');
+              err.message.should.equal('cannot use reserved word in metadata: "' + key + '"');
+              err.error.should.equal(true);
+
+              if (key === 'password') {
+                return db.login('robin', 'dickgrayson').then(function (res) {
+                  res.ok.should.equal(true);
+                }).catch(function (err) {
+                  should.not.exist(err);
+                });
+              } else {
+                return db.getUser('robin').then(function (changedUser) {
+                  changedUser[key].should.deep.equal(user[key]);
+                }).catch(function (err) {
+                  should.not.exist(err);
+                });
+              }
+            });
+          });
+        });
+      });
+    });
+
+    it('Test changing metadata using non-reserved word "metadata"', function () {
+      var metadata = {test: 'test'};
+      return db.signup('robin', 'dickgrayson').then(function (res) {
+        res.ok.should.equal(true);
+        return db.login('robin', 'dickgrayson');
+      }).then(function () {
+        return db.putUser('robin', {metadata: {metadata: metadata}});
+      }).then(function (res) {
+        res.ok.should.equal(true);
+        return db.getUser('robin');
+      }).then(function (changedUser) {
+        changedUser.metadata.should.deep.equal(metadata);
+      }).catch(function (err) {
+        should.not.exist(err);
       });
     });
 
