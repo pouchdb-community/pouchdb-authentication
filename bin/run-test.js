7777492e3aa384407e4f8244005339422671f005
Didier Villevalois (Ptitjes) <ptitjes@free.fr>
2017-11-18 16:01:07 +0100

    build(test): use karma instead of zuul

--- bin/run-test.js
+++ bin/run-test.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var childProcess = require('child_process');
+var karma = require('karma');
 
 var binDir = 'node_modules/.bin/';
 
@@ -9,18 +10,36 @@ var client = process.env.CLIENT || 'phantom';
 
 if (client === 'node') {
   npmRun('mocha', ['--ui', 'bdd', 'test/*']);
-} else if (client === 'phantom') {
-  npm(['install', '--no-save', 'phantomjs-prebuilt']);
-  npmRun('zuul', ['--phantom', '--ui', 'mocha-bdd', 'test/*']);
-} else if (client === 'local') {
-  npmRun('zuul', ['--local', '9000', '--no-coverage', '--ui', 'mocha-bdd', 'test/*']);
-}
+} else {
+  var browsers = {
+    'phantom': ['PhantomJS'],
+    'local': []
+  }[client];
 
-function npm(args) {
-  childProcess.spawnSync('npm', args, {
-    env: process.env,
-    stdio: 'inherit'
+  var server = new karma.Server({
+    basePath: '',
+    frameworks: ['mocha', 'chai', 'browserify'],
+    files: [
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'test/**/*.js': ['browserify']
+    },
+    browserify: {
+      debug: true,
+      transform: ['brfs']
+    },
+    browsers: browsers,
+    phantomJsLauncher: {
+      exitOnResourceError: true
+    },
+    port: 9876,
+    colors: true,
+    reporters: ['mocha'],
+    concurrency: 1,
+    singleRun: client !== 'local'
   });
+  server.start();
 }
 
 function npmRun(bin, args) {
